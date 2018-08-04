d7039af3f9c304c03d36af81bf83a56c5155bc01
Ovidio Calvet <calveto@gmail.com>
2018-08-04 14:48:42 -0400

    Switch counter to use redux

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_redux/main.dart';
+import 'package:flutter_redux_example/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
